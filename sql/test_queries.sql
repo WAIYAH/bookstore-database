7e9f441b5d02756ed3f1f0ba816b7d6d79d39e74
jsolosi <joycesolosi@gmail.com>
2025-04-13 19:32:37 +0300

    Joyce: Added 2 test queries

--- sql/test_queries.sql
+++ sql/test_queries.sql
@@ -11,6 +11,22 @@ FROM publisher p
 JOIN book b ON p.publisher_id = b.publisher_id
 WHERE p.publisher_name = 'Longhorn Publishers';
 
+-- Joyce: Query 3: Customer orders with book details
+SELECT co.order_id, co.order_date, c.first_name, b.title, ol.quantity
+FROM cust_order co
+JOIN customer c ON co.customer_id = c.customer_id
+JOIN order_line ol ON co.order_id = ol.order_id
+JOIN book b ON ol.book_id = b.book_id
+WHERE c.email = 'amina.kamau@gmail.com';
+
+-- Joyce: Query 4: Authors and their books
+SELECT a.author_name, b.title
+FROM author a
+JOIN book_author ba ON a.author_id = ba.author_id
+JOIN book b ON ba.book_id = b.book_id
+WHERE a.author_name = 'Ngũgĩ wa Thiong''o';
+
+
 -- Vanessa: Query 5: Swahili books
 SELECT b.title, bl.language_name
 FROM book b
