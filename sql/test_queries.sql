b173287bc0fab24bcd489a2b716147c22e9b754a
waiya <luckiesdabwoy@gmail.com>
2025-04-13 11:40:48 +0300

    Lucky: Added 2 test queries

---
+++ sql/test_queries.sql
@@ -0,0 +1,12 @@
+-- Lucky: Query 1: Total order value per customer
+SELECT c.first_name, c.last_name, SUM(ol.quantity * ol.price) AS total_spent
+FROM customer c
+JOIN cust_order co ON c.customer_id = co.customer_id
+JOIN order_line ol ON co.order_id = ol.order_id
+GROUP BY c.customer_id;
+
+-- Lucky: Query 2: Books by publisher
+SELECT p.publisher_name, b.title
+FROM publisher p
+JOIN book b ON p.publisher_id = b.publisher_id
+WHERE p.publisher_name = 'Longhorn Publishers';
